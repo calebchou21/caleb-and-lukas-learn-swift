340fdb4f4f44d50513a47b19631e2add222735c6
calebchou21 <calebchou21@gmail.com>
2025-07-03 17:06:37 -0600

    rahhhhhhh

--- random-cat-facts/random-cat-facts/ContentView.swift
+++ random-cat-facts/random-cat-facts/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+struct CatFact: Codable {
+    let fact: String
+    let length: Int
+}
+
+enum CatFactError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
+
 struct ContentView: View {
+    @State var catFact: String?
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(catFact ?? "Press the button to get a cat fact!")
+            Button(action: {
+                Task {
+                    catFact = try await fetchCatFact().fact
+                }
+            }) {
+                Text("New Cat Fact")
+            }
         }
         .padding()
+    }
+    
+    func fetchCatFact() async throws -> CatFact {
+        guard let url = URL(string: "https://catfact.ninja/fact") else {
+            throw CatFactError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw CatFactError.invalidResponse
+        }
+        
+        do {
+            return try JSONDecoder().decode(CatFact.self, from: data)
+        } catch {
+            throw CatFactError.invalidData
+        }
     }
 }
 
