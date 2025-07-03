be421cfcf54b2cf67d1aa6c3515633ffebe798af
calebchou21 <calebchou21@gmail.com>
2025-07-03 16:08:21 -0600

    initial commit

---
+++ random-cat-facts/random-cat-facts/random_cat_factsApp.swift
@@ -0,0 +1,17 @@
+//
+//  random_cat_factsApp.swift
+//  random-cat-facts
+//
+//  Created by caleb chou on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct random_cat_factsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
