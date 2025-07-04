df357ecd35f57be270b0e86e1d5bc0c981411aee
calebchou21 <calebchou21@gmail.com>
2025-07-04 12:47:55 -0600

    api call

--- Trivia/Trivia/ContentView.swift
+++ Trivia/Trivia/ContentView.swift
@@ -1,21 +1,36 @@
-//
-//  ContentView.swift
-//  Trivia
-//
-//  Created by Lukas Chou on 7/3/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var speed = 50.0
+    @State private var isEditing = false
+    @State private var triviaQuestions: [TriviaQuestion]?
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let questions = triviaQuestions, !questions.isEmpty {
+                Text(questions[0].question)
+            } else {
+                Text("Hello World")
+            }
+            Slider(
+                value: $speed,
+                in: 0...100,
+                onEditingChanged: { editing in
+                    isEditing = editing
+                }
+            )
         }
         .padding()
+        .task {
+            do {
+                triviaQuestions = try await fetchTriviaData().results
+            } catch {
+                print("Failed to fetch trivia")
+            }
+        }
     }
 }
 
