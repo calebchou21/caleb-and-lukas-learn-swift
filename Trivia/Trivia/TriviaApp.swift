422041e0857dda33ca6ffdc401f560b9eaab2191
Lukas Chou <lukaschou21@icloud.com>
2025-07-03 21:28:38 -0700

    First commit

---
+++ Trivia/Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Lukas Chou on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Trivia/TriviaTests/TriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaTests.swift
+//  TriviaTests
+//
+//  Created by Lukas Chou on 7/3/25.
+//
+
+import Testing
+@testable import Trivia
+
+struct TriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
